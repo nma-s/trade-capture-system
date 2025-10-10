0a4b295604d2effbef28d84492b6b516e1102131
nsamuels <n-samuels@outlook.com>
2025-10-10 12:52:19 +0100

    fix(test): TradeControllerTest - Fixed testUpdateTrade to stub correct methods
    - Problem: testUpdateTrade() failed with an empty body causing error: No value at JSON path $.tradeId
    - Root Cause: The test was the same as testCreateTrade which stubbed tradeService.saveTrade() which is not directly called by updateTrade. The test did not stub tradeService.amendTrade() and tradeMapper.toDto() so the responseDTO was null
    - Solution: Stubbed tradeService.amendTrade() and tradeMapper.toDto(trade) and removed doNothing.when(tradeService).populateReferenceDataByName()
    - Impact: The updateTrade receives a non-empty JSON response with tradeId

--- backend/src/test/java/com/technicalchallenge/controller/TradeControllerTest.java
+++ backend/src/test/java/com/technicalchallenge/controller/TradeControllerTest.java
@@ -183,8 +183,10 @@ public class TradeControllerTest {
         // Given
         Long tradeId = 1001L;
         tradeDTO.setTradeId(tradeId);
-        when(tradeService.saveTrade(any(Trade.class), any(TradeDTO.class))).thenReturn(trade);
-        doNothing().when(tradeService).populateReferenceDataByName(any(Trade.class), any(TradeDTO.class));
+//        when(tradeService.saveTrade(any(Trade.class), any(TradeDTO.class))).thenReturn(trade);
+//        doNothing().when(tradeService).populateReferenceDataByName(any(Trade.class), any(TradeDTO.class));
+            when(tradeService.amendTrade(eq(tradeId), any(TradeDTO.class))).thenReturn(trade);
+            when(tradeMapper.toDto(trade)).thenReturn(tradeDTO);
 
         // When/Then
         mockMvc.perform(put("/api/trades/{id}", tradeId)
@@ -193,7 +195,9 @@ public class TradeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.tradeId", is(1001)));
 
-        verify(tradeService).saveTrade(any(Trade.class), any(TradeDTO.class));
+//      NS: changed stubs to call methods in updateTrade()
+        verify(tradeService).amendTrade(eq(tradeId), any(TradeDTO.class));
+        verify(tradeMapper).toDto(trade);
     }
 
     @Test
